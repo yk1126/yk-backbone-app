f2d1b4b4ed6afba5b425563d5ea70e8ec03466b1
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2013-08-29 19:24:16 +0530

    Commit 2: Try Adding Local Server

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 var Msg = Backbone.Model.extend({
-    defaults: {desc: 'Sample message'}
+    defaults: {desc: 'Sample message'},
+  //  localStorage: new Backbone.LocalStorage('msg-backbone')
 });
 
 var msg = new Msg({desc: 'This is my first message.'});
 
+msg.on('change', function() {console.log("Data changed");});
 
 var MsgView = Backbone.View.extend({
-    el: '#msgs',
+    el: '#msgs',     //Set an existing element with id '#msgs'
+    myList: _.template('<li>Msg: "<%= desc %>"</li>'),
     render: function() {
-	var html = '<li>' + this.model.get('desc') + '</li>';
-	console.log(this.model.get('desc'));
-	console.log(html);
-	console.log(this.el);
-	$(this.el).html(html);
+	//var html = '<li>' + this.model.get('desc') + '</li>';
+	//console.log(this.model.get('desc'));
+	//console.log(html);
+	//console.log(this.el);
+	var attributes = this.model.toJSON();
+	this.$el.html(this.myList(attributes));
     }
 });
 
 var msgView = new MsgView({model: msg});
+
+
