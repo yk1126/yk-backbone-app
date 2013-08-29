0aff22e372454eb0fcc7fa440d2c6e11ff344282
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2013-08-29 19:43:19 +0530

    Commit 3: Set 'read' status from Checkbox Data Change

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 var Msg = Backbone.Model.extend({
-    defaults: {desc: 'Sample message'},
-  //  localStorage: new Backbone.LocalStorage('msg-backbone')
+    defaults: {desc: 'Sample message', read: false},
+    //  localStorage: new Backbone.LocalStorage('msg-backbone')
+    toggleRead: function() {
+	if(this.get('read'))
+	    this.set({read: false});
+	else
+	    this.set({read: true});
+    }
 });
 
 var msg = new Msg({desc: 'This is my first message.'});
@@ -9,7 +15,11 @@ msg.on('change', function() {console.log("Data changed");});
 
 var MsgView = Backbone.View.extend({
     el: '#msgs',     //Set an existing element with id '#msgs'
-    myList: _.template('<li>Msg: "<%= desc %>"</li>'),
+    myList: _.template('<li><input type=checkbox id="listedMsg"' + '<% if(read) print("checked") %>/>' + 'Msg: "<%= desc %>"</li>'),
+    events: {
+	'dblclick li': 'alertClick',
+	'change input[id="listedMsg"]': 'toggleRead'
+    },
     render: function() {
 	//var html = '<li>' + this.model.get('desc') + '</li>';
 	//console.log(this.model.get('desc'));
@@ -17,6 +27,12 @@ var MsgView = Backbone.View.extend({
 	//console.log(this.el);
 	var attributes = this.model.toJSON();
 	this.$el.html(this.myList(attributes));
+    },
+    alertClick: function(event) {
+	console.log("Double click on 'li' element");
+    },
+    toggleRead: function() {
+	this.model.toggleRead();
     }
 });
 
